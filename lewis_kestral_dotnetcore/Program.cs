1184ffc9b940890d670326b25bb809a4c4661d60
Seann Alexander <self@seannalexander.com>
2019-02-24 17:43:31 -0500

    add kestral support

--- lewis_kestral_dotnetcore/Program.cs
+++ lewis_kestral_dotnetcore/Program.cs
@@ -14,12 +14,38 @@ namespace lewis_kestral_dotnetcore
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            /*CreateWebHostBuilder(args)
+            .Build()
+            .Run();
+            
+            /*
+            var builder = new WebHostBuilder()
+                .UseWebListener(options =>
+                {
+                    options.MaxAccepts = 1000;   
+                }
+                );
+            builder().Build().Run();
+            */
+            
         }
 
+        /*
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
                 ;
-    }
+                */
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .UseKestrel(options =>
+                {
+                    // The following options are set to default values.
+                    options.MaxConnections = null;
+                    options.MaxRequestBodySize = 30000000;
+                    options.UrlPrefixes.Add("http://localhost:5000");
+                });
+            }
 }
